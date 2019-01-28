90c594f9a4261cc1946c2f78bf0fc192ab01a77e
insanrizky <muhamad.rizky@bizzy.co.id>
2019-01-28 20:31:47 +0700

    fix origin

--- config/env/production.js
+++ config/env/production.js
@@ -255,6 +255,7 @@ module.exports = {
     * > Be sure to use the right protocol!  ("http://" vs. "https://")         *
     *                                                                          *
     ***************************************************************************/
+    onlyAllowOrigins: [],
     // onlyAllowOrigins: [
     //   'http://localhost:3000',
     //   'https://backend-evolve.herokuapp.com',
